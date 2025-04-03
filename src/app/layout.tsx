cafc35f6bf3b54774f28d7716a324c2be71f5969
bipulk69 <bknor951@gmail.com>
2025-04-03 20:57:04 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'The Virasat',
   description: "Discover India's Rich Cultural Heritage",
+  openGraph: {
+    title: 'The Virasat',
+    description: "Explore India's rich cultural heritage, art, and traditions.",
+    url: 'https://thevirasat.com', // Make sure to use your actual domain here
+    siteName: 'The Virasat',
+  },
 };
 
 export default function RootLayout({
